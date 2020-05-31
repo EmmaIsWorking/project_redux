d386ba49c2dc9155b44373b9f56dbe04dfecb9cb
EmmaIsWorking <dimple.pleod@gmail.com>
2020-05-30 22:09:55 -0500

    fin del proyecto

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Menu from './Menu';
 import Usuarios from './usuarios/Usuarios';
 import Publicaciones from './Publicaciones/index';
-
-const Prueba = () => <div>hola</div>;
+import Tareas from './Tareas/';
+import TareasGuardar from './Tareas/Guardar';
 
 const App = (props) => (
 	<BrowserRouter>
 		<Menu />
 		<div id='margen'>
 			<Route exact path='/' component={Usuarios} />
-			<Route exact path='/tareas' component={Prueba} />
+			<Route exact path='/tareas' component={Tareas} />
 			<Route exact path='/publicaciones/:key' component={Publicaciones} />
+			<Route exact path='/tareas/guardar' component={TareasGuardar} />
+			<Route
+				exact
+				path='/tareas/guardar/:usu_id/:tar_id'
+				component={TareasGuardar}
+			/>
 		</div>
 	</BrowserRouter>
 );
